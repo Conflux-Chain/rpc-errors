72e779fd40dad4c1f33ac1d85677333b510b7f30
iosh <ioshx@foxmail.com>
2025-01-10 16:59:02 +0800

    chore: add cfx_getStorageAt

--- packages/rpc-errors/src/coreSpace/invalidParamsErrors/hash.ts
+++ packages/rpc-errors/src/coreSpace/invalidParamsErrors/hash.ts
@@ -7,7 +7,7 @@ export type InvalidHashTypeErrorType = {
 export class InvalidHashTypeError extends InvalidParamsError {
   override name = "InvalidHashType";
   static pattern =
-    /expected a \(both 0x-prefixed or not\) hex string with length of 64/;
+    /expected a \(both 0x-prefixed or not\) hex string with length/;
   constructor(message: string,  data?: any) {
     super(message, data);
   }
